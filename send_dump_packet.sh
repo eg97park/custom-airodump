c8a97aabf1e3ff5937fc7c12c0c25eeea539083b
Eungyu Park <ekpark97.dev@gmail.com>
2023-01-10 11:33:35 +0000

    project: send packet script

--- send_dump_packet.sh
+++ send_dump_packet.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-sudo tcpreplay -i wlan0 ./sample_packets/*.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/*a2000ua*.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/*awus051nh*.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/*galaxy7*.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/*a2000ua*.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/*nexus5*.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/*forcerecon*.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/*dot11*.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/80211-icmp.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/80211-sample2.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/80211-sample.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/80211-sample1.pcap
+sudo tcpreplay -i wlan0 ./sample_packets/80211-sample3.pcap
